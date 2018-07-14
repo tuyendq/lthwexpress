b135d15c6b1274f6276dcb73c461ec192a66ca72
Tuyen Dang <tuyendq@gmail.com>
2018-07-14 10:27:24 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+import express from 'express';
+import favicon from 'serve-favicon';
+import path from 'path';
+import data from './data/data.json';
+
+const app = express();
+const PORT = 3000;
+
+// This is for the public folder on path /
+app.use(express.static('public'));
+
+// method to use JSON
+// app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// This is for proxies
+app.set('trust proxy', 'loopback');
+
+// This is for images folder on path images
+app.use('/images', express.static('images'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+
+app.get('/', (req, res) =>
+    // res.send(`a get request with / route on port ${PORT}`)
+    // get date first
+    res.json(data)
+);
+
+// JSON data
+// { "hello": "JSOn is cool"}
+// URLEncoded data
+// hello=URLEncoded+is+cool
+
+app.post('/newItem', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+}
+)
+
+
+
+app.get('/item/:id', (req, res, next) => {
+    // this is the middleware that pulls the data
+    console.log(req.params.id);
+    let user = Number(req.params.id);
+    console.log(user);
+    console.log(data[user]);
+    // middleware that uses the req object
+    console.log(`Request from: ${req.originalUrl}`);
+    console.log(`Request type: ${req.method}`);
+    // everything above is middleware
+    res.send(data[user]);
+    next();
+}, (req, res) => {
+    console.log('Did you get the right data?');
+}
+);
+
+/* app.put('/item', (req, res) =>
+    res.send(`a put request with /newItem route on port ${PORT}`)
+); */
+
+
+
+app.route('/item')
+    .get((req, res) => {
+        throw new Error();
+        // res.redirect('http://www.linkedin.com')
+        //res.download('images/test2.jpg')
+        // res.end()
+        // res.send(`a get request on port ${PORT}`)
+    })
+    .post((req, res) => 
+        res.send(`a post request with /item route on port ${PORT}`)
+    )
+    .put((req, res) => 
+        res.send(`a put request with /item route on port ${PORT}`)
+    )
+    .delete((req, res) =>
+        res.send(`a delete request with /item route on port ${PORT}`)
+    );
+
+// Error handling function
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send(`Red alert! Red alert!: ${err.stack}`);
+}); 
+
+app.listen(PORT, () => {
+    console.log(`Your server is running on port ${PORT}`);
+    // console.log(data);
+});
